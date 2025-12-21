76e4e6c799ea94b5531bd15433b176998e4ce840
devwraithe <folahanmi0001@gmail.com>
2025-12-21 16:59:00 +0100

    chore: complete 06_move_semantics exercise

--- exercises/06_move_semantics/move_semantics2.rs
+++ exercises/06_move_semantics/move_semantics2.rs
@@ -20,7 +20,9 @@ mod tests {
     fn move_semantics2() {
         let vec0 = vec![22, 44, 66];
 
-        let vec1 = fill_vec(vec0);
+        // let vec1: Vec<i32> = fill_vec(&vec0);
+
+        let vec1: Vec<i32> = fill_vec(vec0.clone()); // cloned to take ownership
 
         assert_eq!(vec0, [22, 44, 66]);
         assert_eq!(vec1, [22, 44, 66, 88]);
