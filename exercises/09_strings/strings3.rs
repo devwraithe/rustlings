2118f2e1b029c3f2ab1b369c7229ec311db3434e
devwraithe <folahanmi0001@gmail.com>
2025-12-21 18:08:58 +0100

    chore: complete 09_strings exercise

--- exercises/09_strings/strings3.rs
+++ exercises/09_strings/strings3.rs
@@ -1,13 +1,21 @@
 fn trim_me(input: &str) -> &str {
     // TODO: Remove whitespace from both ends of a string.
+    input.trim()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There are multiple ways to do this.
+    let mut new_input = String::from(input);
+    let word_to_add = " world!";
+
+    new_input.push_str(word_to_add);
+
+    new_input
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons".
+    input.replace("cars", "balloons")
 }
 
 fn main() {
